7abb2f761e720982954c13cdcc84a326ce54f0d2
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2026-02-11 17:10:12 +0000

    Add gameboard IIFE with a private variable for the board

---
+++ script.js
@@ -0,0 +1,15 @@
+const gameboard = (function CreateGameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i][j] = null;
+        };
+    };
+
+    const getBoard = () => board;
+    return {getBoard};
+})();
